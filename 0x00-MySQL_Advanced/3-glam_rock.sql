757034bf6a5dfa9615a28fb0096b6132089247f3
Roo-rabab <rababmchich312@gmail.com>
2024-02-22 14:48:17 -0800

    MySQL_Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,20 +1,5 @@
--- Create a temporary table to store the split and formed attributes
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_band_info AS (
-    SELECT
-        band_name,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(attributes, 'formed:', -1), ',', 1) AS formed,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(attributes, 'split:', -1), ',', 1) AS split
-    FROM
-        bands
-);
-
--- Calculate the lifespan in years until 2022
-SELECT
-    band_name,
-    IF(split = 0, 0, IF(split = -1, 2022 - formed, split - formed)) AS lifespan
-FROM
-    temp_band_info
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
