5da71d654bb924ea3081731d8819f37f7696d280
Roo-rabab <rababmchich312@gmail.com>
2024-02-28 11:35:58 -0800

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of
-the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it.
-
-Start in a new file named web.py and do not reuse the code
-written in exercise.py.
-
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the result with
-an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
-
-
+""" Site Cache """
 import redis
 import requests
 from functools import wraps
+from typing import Callable
 
-r = redis.Redis()
-
-
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
-    return wrapper
+rds = redis.Redis()
 
 
-@url_access_count
+def cache_url(exp_time: int) -> Callable[[Callable], Callable]:
+    """
+    Decorator function for caching the result of a URL request in Redis.
+    """
+    def decorator(method: Callable) -> Callable:
+        """ decorator function """
+
+        @wraps(method)
+        def wrapper(url: str) -> str:
+            """
+            Wrapper function to check and retrieve the result from the
+            cache or make a new request.
+            """
+            count = "count:{}".format(url)
+            key = "result:{}".format('url')
+
+            rds.incr(count)
+
+            result = rds.get(key)
+
+            if result:
+                return result.decode('utf-8')
+
+            res = method(url)
+            rds.set(count, 0)
+            rds.setex(key, exp_time, res)
+
+            return res
+
+        return wrapper
+
+    return decorator
+
+
+@cache_url(10)
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
+    """
+    Retrieve the content of a web page and cache the result.
+    """
+    resp = requests.get(url)
 
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return "Unable to access {}. Status code: {}".format(
+                url, resp.status_code)
