3d6a533935faf034e61c6128d67cd360ffe4cfdb
Roo-rabab <rababmchich312@gmail.com>
2024-02-28 05:54:54 -0800

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,39 @@
-#!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+# web.py
 import requests
 import redis
-from functools import wraps
+import time
 
 store = redis.Redis()
 
-
 def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
+        count_key = f"count:{url}"
         store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        return method(url)
+
     return wrapper
 
+def cache_with_expiration(expiration_time):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(url):
+            cached_key = f"cached:{url}"
+            cached_data = store.get(cached_key)
+            if cached_data:
+                return cached_data.decode("utf-8")
+
+            html = func(url)
+
+            store.setex(cached_key, expiration_time, html)
+            return html
+
+        return wrapper
+
+    return decorator
 
 @count_url_access
+@cache_with_expiration(expiration_time=10)
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    response = requests.get(url)
+    return response.text
