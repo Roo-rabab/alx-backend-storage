04d90ac62e83e11c40595066901633bfd51c37fa
Roo-rabab <rababmchich312@gmail.com>
2024-02-28 05:15:32 -0800

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
 from functools import wraps
+from typing import Callable
 
-# Use a connection pool for Redis
-pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
-store = redis.Redis(connection_pool=pool)
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url):
-        cached_key = f"cached:{url}"
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-        count_key = f"count:{url}"
-        try:
-            html = method(url)
-            store.incr(count_key)
-            store.setex(cached_key, 10, html)  # Use setex for setting expiration
-            return html
-        except requests.RequestException as e:
-            # Handle request exceptions here
-            print(f"Error fetching URL: {url}. Exception: {e}")
-            return ""
-    return wrapper
 
-@count_url_access
+@data_cacher
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    res.raise_for_status()  # Raise HTTPError for bad responses
-    return res.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
