661af0b0c29201c2048594c11ed9362552d6e5b2
Roo-rabab <rababmchich312@gmail.com>
2024-02-28 05:57:57 -0800

    redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,21 @@
-# web.py
-import requests
+#!/usr/bin/env python3
+"""
+create a web cach
+"""
 import redis
-import time
+import requests
+rc = redis.Redis()
+count = 0
 
-store = redis.Redis()
 
-def count_url_access(method):
-    @wraps(method)
-    def wrapper(url):
-        count_key = f"count:{url}"
-        store.incr(count_key)
-        return method(url)
-
-    return wrapper
-
-def cache_with_expiration(expiration_time):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url):
-            cached_key = f"cached:{url}"
-            cached_data = store.get(cached_key)
-            if cached_data:
-                return cached_data.decode("utf-8")
-
-            html = func(url)
-
-            store.setex(cached_key, expiration_time, html)
-            return html
-
-        return wrapper
-
-    return decorator
-
-@count_url_access
-@cache_with_expiration(expiration_time=10)
 def get_page(url: str) -> str:
-    response = requests.get(url)
-    return response.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
