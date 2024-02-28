df8d8eab661b43198b812fcdd69b61807104e620
Roo-rabab <rababmchich312@gmail.com>
2024-02-28 11:30:06 -0800

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,16 @@
 #!/usr/bin/env python3
-'''A module with tools for expiring request caching and tracking.
-'''
+"""Implements a get page function"""
 import redis
 import requests
-from functools import wraps
-from typing import Callable
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-def expiring_data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data with expiration.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output with expiration.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
 
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    tracking the request, and using an expiration time.
-    '''
-    return requests.get(url).text
+    """Tracks the amount of times a url is called"""
+    r = redis.Redis()
+    key = 'count:' + url
+    if not r.get(key):
+        r.setex('count:'+url, 10, 1)
+    else:
+        r.incr(key, 1)
 
-# Decorate the get_page function with the expiring_data_cacher decorator
-get_page = expiring_data_cacher(get_page)
-
-# Test the decorated function
-if __name__ == "__main__":
-    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
-    print(get_page(slow_url))
+    return requests.get(url).content.decode()
